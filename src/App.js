4b094c88eeef4d82a09921e0de7d1a9d04f939bc
Boa <boa.matule@gmail.com>
2020-08-19 04:26:05 +0200

    Implements NotFoundPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -9,18 +10,22 @@ import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NavBar from './NavBar';
 import './App.css';
+import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
   render() {
     return (
-      <Router> 
+      <Router>
         <div className="App">
           <NavBar />
           <div id="page-body">
+            <Switch>
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
             <Route path="/articles-list" component={ArticlesListPage} />
             <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage}/>
+          </Switch>
           </div>
         </div>
       </Router>
