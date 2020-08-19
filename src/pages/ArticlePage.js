fe60641c184e822dc08420df100f1a6dba51ae4f
Boa <boa.matule@gmail.com>
2020-08-19 03:26:22 +0200

    Creates an article-content.js and populates with data to be displayed using match and find.; Create a statement to return info if the article searched is not available.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import articleContent from './article-content'
+import articles from './article-content';
 
-const ArticlePage = () => (
-  <>
-    <hi>This is an article</hi>   
-  </>
-);
+const ArticlePage = ({match}) => {
+  const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
+
+  if (!article) return <h1>Article does not exist!</h1>
+  return (
+    <>
+      <hi>{article.title}</hi>   
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
+  );
+}
 
 export default ArticlePage;
