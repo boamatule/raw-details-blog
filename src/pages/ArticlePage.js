9b515ea62d12116ce31ad7765d9a8a24761d0ed5
Boa <boa.matule@gmail.com>
2020-08-28 18:06:18 +0200

    Implements the CommentForm Component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import CommentsList from '../components/CommentsList';
 import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
@@ -13,13 +14,12 @@ const ArticlePage = ({match}) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch(`api/articles/${name}`);
+      const result = await fetch(`/api/articles/${name}`);
       const body = await result.json();
-      console.log(body);
+      // console.log(body);
       setArticleInfo(body);
     }
     fetchData();
-    // setArticleInfo({ upvotes: Math.ceil(Math.random() * 10)});
   }, [name]);
 
   if (!article) return <NotFoundPage />
@@ -36,6 +36,7 @@ const ArticlePage = ({match}) => {
         <p key={key}>{paragraph}</p>
       ))}
       <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
